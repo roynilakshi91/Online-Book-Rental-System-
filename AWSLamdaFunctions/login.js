d531a90360a918c37770ea94f0661f755b184de1
roynilakshi91 <82709191+roynilakshi91@users.noreply.github.com>
2022-04-14 21:09:21 -0700

    Create login.js

---
+++ AWSLamdaFunctions/login.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
+
+
+exports.handler = (event, context, callback) => {
+    var found = "Username or password is incorrect" ;
+    const getparams = {
+        TableName: "users",
+        Key:"id"
+    };
+
+    docClient.scan(getparams, onScan);
+
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+
+            console.log("Scan succeeded.");
+
+            data.Items.forEach(function(users) {
+
+                console.log(users.username) ;
+                if(event.username===users.username)
+                {
+                    console.log("Inside If.");
+                    found = users ;
+
+                }
+
+            });
+
+        }
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Content-Type" : "application/json",
+                "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods" : "OPTIONS,GET",
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+                "X-Requested-With" : "*"
+            },
+            body: found,
+        };
+
+        callback(null, response);
+    }
+};
