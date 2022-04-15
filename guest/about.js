3df54274d095d33e7997a5d78328bb4fff0c7274
roynilakshi91 <82709191+roynilakshi91@users.noreply.github.com>
2022-04-14 21:14:29 -0700

    Create about.js

---
+++ guest/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function About(props){
+
+return <div>
+	<main className ="box">
+		<header>
+			<h1 className="fh-custom-font"> Book Rental System</h1>
+			<h2> About Us</h2>
+		</header>
+		<p>The Book Rental System was established in 2021. We offer a vast range of books from different genre's. </p>
+		<p> Anyone can rent a book using our system and get it delivered to thier address and return it whenever they are done.</p>
+		<p> This is the most convenient way of reusing books as well as provides every book reading enthusiast the opportunity to explore.</p>
+
+
+
+	</main>
+	<footer>&#127926;&copy; Copyright Book Rental &#127925;</footer>
+</div>
+}
+
+export default About;
